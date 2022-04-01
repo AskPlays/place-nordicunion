776c8ddd5072a1339e6f36c8cb8086ea77ea3e83
AskPlays <adrian.skalevag@online.no>
2022-04-02 01:50:26 +0200

    changed image link

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
           document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
       (function () {
           const i = document.createElement("img");
-          i.src = "https://raw.githubusercontent.com/AskPlays/place-nordicunion/505151e1532c8c4ce895cce635d1f00da4446f91/dotted-place-template.png";
+          i.src = "https://raw.githubusercontent.com/AskPlays/place-nordicunion/main/dotted-place-template.png";
           i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
           console.log(i);
           return i;
