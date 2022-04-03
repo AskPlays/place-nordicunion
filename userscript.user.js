f10cdc57d91f01cd64bbe37931bb60815ee244f4
AskPlays <adrian.skalevag@online.no>
2022-04-03 18:25:43 +0200

    added download url

--- userscript.user.js
+++ userscript.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name         Nordic Union template
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to defend the nordic union!
 // @author       AskPlays
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/AskPlays/place-nordicunion/raw/main/userscript.user.js
+// @downloadURL  https://github.com/AskPlays/place-nordicunion/raw/main/userscript.user.js
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
   window.addEventListener('load', () => {
-          document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+      document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
       (function () {
           const i = document.createElement("img");
           i.src = "https://raw.githubusercontent.com/AskPlays/place-nordicunion/main/dotted-place-template.png";
