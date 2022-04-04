0bad42a475a9dd301bb8cc7e7751efb11a445229
AskPlays <adrian.skalevag@online.no>
2022-04-04 18:39:08 +0200

    fixed i

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nordic Union template
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  try to defend the nordic union!
 // @author       AskPlays
 // @match        https://hot-potato.reddit.com/embed*
@@ -16,7 +16,9 @@ if (window.top !== window.self) {
       (function () {
           const i = document.createElement("img");
           i.src = "https://raw.githubusercontent.com/AskPlays/place-nordicunion/main/dotted-place-template.png";
-          i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: ${i.width/3}px; height: ${i.height/3}px;";
+          i.onload = () => {
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: ${i.width/3}px; height: ${i.height/3}px;";
+          }
           console.log(i);
           return i;
       })())
